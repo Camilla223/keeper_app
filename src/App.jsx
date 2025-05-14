f82b1ad5a5a6b5f4f3ec12421f5dab1544168d5e
Camilla Toso <39516759+Camilla223@users.noreply.github.com>
2025-05-14 14:02:06 +0000

    create dynamic notes

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 import Note from "./components/Note.jsx";
+import notes from "./notes.js";
+
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map((note) => {
+        return <Note key={note.id} title={note.title} content={note.content} />;
+      })}
+
       <Footer />
     </div>
   );
